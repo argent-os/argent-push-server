90ba154b17b71fe48233fb55914ee29e08a9249c
Sinan Ulkuatam <sulkuatam@gmail.com>
2016-05-14 18:14:23 -0400

    add breaks to switch statement

--- stripe-webhook-middleware.js
+++ stripe-webhook-middleware.js
@@ -87,111 +87,164 @@ function StripeWebhook (options, app) {
               var device_token = obj["ios"]["device_token"];             
               switch(evt) {
                 case "invoice.created":
-                   notify.sendPushNotification("Invoice created", device_token);            
+                   notify.sendPushNotification("Invoice created", device_token);
+                   break;            
                 case "account.updated":
-                   notify.sendPushNotification("Account updated", device_token);            
+                   notify.sendPushNotification("Account updated", device_token);
+                   break;            
                 case "account.application.deauthorized":
-                   notify.sendPushNotification("Account application deauthorized", device_token); 
+                   notify.sendPushNotification("Account application deauthorized", device_token);
+                   break; 
                 case "account.external_account.created":
-                   notify.sendPushNotification("External account created", device_token);            
+                   notify.sendPushNotification("External account created", device_token);
+                   break;            
                 case "account.external_account.updated":
-                   notify.sendPushNotification("External account updated", device_token);            
+                   notify.sendPushNotification("External account updated", device_token);
+                   break;            
                 case "account.external_account.deleted":
-                   notify.sendPushNotification("External account deleted", device_token);            
+                   notify.sendPushNotification("External account deleted", device_token);
+                   break;            
                 case "application_fee.created":
-                   notify.sendPushNotification("Application fee created", device_token);            
+                   notify.sendPushNotification("Application fee created", device_token);
+                   break;            
                 case "application_fee.refunded":
-                   notify.sendPushNotification("Application fee refunded", device_token);            
+                   notify.sendPushNotification("Application fee refunded", device_token);
+                   break;            
                 case "balance.available":
-                   notify.sendPushNotification("Your balance is now available", device_token);            
+                   notify.sendPushNotification("Your balance is now available", device_token);
+                   break;            
                 case "charge.succeeded":
-                   notify.sendPushNotification("Charge succeeded", device_token);            
+                   notify.sendPushNotification("Charge succeeded", device_token);
+                   break;            
                 case "charge.failed":
-                   notify.sendPushNotification("Charge failed", device_token);            
+                   notify.sendPushNotification("Charge failed", device_token);
+                   break;            
                 case "charge.refunded":
-                   notify.sendPushNotification("Charge refunded", device_token);            
+                   notify.sendPushNotification("Charge refunded", device_token);
+                   break;            
                 case "charge.captured":
-                   notify.sendPushNotification("Charge captured", device_token);            
+                   notify.sendPushNotification("Charge captured", device_token);
+                   break;            
                 case "charge.updated":
-                   notify.sendPushNotification("Charge updated", device_token);            
+                   notify.sendPushNotification("Charge updated", device_token);
+                   break;            
                 case "charge.dispute.created":
-                   notify.sendPushNotification("Charge dispute created", device_token);            
+                   notify.sendPushNotification("Charge dispute created", device_token);
+                   break;            
                 case "charge.dispute.updated":
-                   notify.sendPushNotification("Charge dispute updated", device_token);            
+                   notify.sendPushNotification("Charge dispute updated", device_token);
+                   break;            
                 case "charge.dispute.closed":
-                   notify.sendPushNotification("Charge dispute closed", device_token);            
+                   notify.sendPushNotification("Charge dispute closed", device_token);
+                   break;            
                 case "customer.created":
-                   notify.sendPushNotification("Customer created", device_token);            
+                   notify.sendPushNotification("Customer created", device_token);
+                   break;            
                 case "customer.updated":
-                   notify.sendPushNotification("Customer updated", device_token);            
+                   notify.sendPushNotification("Customer updated", device_token);
+                   break;            
                 case "customer.deleted":
-                   notify.sendPushNotification("Customer deleted", device_token);            
+                   notify.sendPushNotification("Customer deleted", device_token);
+                   break;            
                 case "customer.card.created":
-                   notify.sendPushNotification("Customer card created", device_token);            
+                   notify.sendPushNotification("Customer card created", device_token);
+                   break;            
                 case "customer.card.updated":
-                   notify.sendPushNotification("Customer card updated", device_token);            
+                   notify.sendPushNotification("Customer card updated", device_token);
+                   break;            
                 case "customer.card.deleted":
-                   notify.sendPushNotification("Customer card deleted", device_token);            
+                   notify.sendPushNotification("Customer card deleted", device_token);
+                   break;            
                 case "customer.subscription.created":
-                   notify.sendPushNotification("Customer subscription created", device_token);            
+                   notify.sendPushNotification("Customer subscription created", device_token);
+                   break;            
                 case "customer.subscription.updated":
-                   notify.sendPushNotification("Customer subscription updated", device_token);            
+                   notify.sendPushNotification("Customer subscription updated", device_token);
+                   break;            
                 case "customer.subscription.deleted":
-                   notify.sendPushNotification("Customer subscription deleted", device_token);            
+                   notify.sendPushNotification("Customer subscription deleted", device_token);
+                   break;            
                 case "customer.subscription.trial_will_end":
-                   notify.sendPushNotification("Customer subscription trial will end soon", device_token);            
+                   notify.sendPushNotification("Customer subscription trial will end soon", device_token);
+                   break;            
                 case "customer.discount.created":
-                   notify.sendPushNotification("Customer discount created", device_token);            
+                   notify.sendPushNotification("Customer discount created", device_token);
+                   break;            
                 case "customer.discount.updated":
-                   notify.sendPushNotification("Customer discount updated", device_token);            
+                   notify.sendPushNotification("Customer discount updated", device_token);
+                   break;            
                 case "customer.discount.deleted":
-                   notify.sendPushNotification("Customer discount deleted", device_token);            
+                   notify.sendPushNotification("Customer discount deleted", device_token);
+                   break;            
                 case "invoice.created":
-                   notify.sendPushNotification("Invoice created", device_token);            
+                   notify.sendPushNotification("Invoice created", device_token);
+                   break;            
                 case "invoice.updated":
-                   notify.sendPushNotification("Invoice updated", device_token);            
+                   notify.sendPushNotification("Invoice updated", device_token);
+                   break;            
                 case "invoice.payment_succeeded":
-                   notify.sendPushNotification("Invoice payment succeeded", device_token);            
+                   notify.sendPushNotification("Invoice payment succeeded", device_token);
+                   break;            
                 case "invoice.payment_failed ":
-                   notify.sendPushNotification("Invoice payment failed", device_token);            
+                   notify.sendPushNotification("Invoice payment failed", device_token);
+                   break;            
                 case "invoiceitem.created":
-                   notify.sendPushNotification("Invoice item created", device_token);            
+                   notify.sendPushNotification("Invoice item created", device_token);
+                   break;            
                 case "invoiceitem.updated":
-                   notify.sendPushNotification("Invoice item updated", device_token);            
+                   notify.sendPushNotification("Invoice item updated", device_token);
+                   break;            
                 case "invoiceitem.deleted":
-                   notify.sendPushNotification("Invoice item deleted", device_token);            
+                   notify.sendPushNotification("Invoice item deleted", device_token);
+                   break;            
                 case "plan.created":
-                   notify.sendPushNotification("Plan created", device_token);            
+                   notify.sendPushNotification("Plan created", device_token);
+                   break;            
                 case "plan.updated":
-                   notify.sendPushNotification("Plan updated", device_token);            
+                   notify.sendPushNotification("Plan updated", device_token);
+                   break;            
                 case" plan.deleted":
-                   notify.sendPushNotification("Plan deleted", device_token);            
+                   notify.sendPushNotification("Plan deleted", device_token);
+                   break;            
                 case "coupon.created":
-                   notify.sendPushNotification("Coupon created", device_token);            
+                   notify.sendPushNotification("Coupon created", device_token);
+                   break;            
                 case "coupon.deleted":
-                   notify.sendPushNotification("Coupon deleted", device_token);            
+                   notify.sendPushNotification("Coupon deleted", device_token);
+                   break;            
                 case "recipient.created":
-                   notify.sendPushNotification("Recipient created", device_token);            
+                   notify.sendPushNotification("Recipient created", device_token);
+                   break;            
                 case "recipient.updated":
-                   notify.sendPushNotification("Recipient updated", device_token);            
+                   notify.sendPushNotification("Recipient updated", device_token);
+                   break;            
                 case "recipient.deleted":
-                   notify.sendPushNotification("Recipient deleted", device_token);            
+                   notify.sendPushNotification("Recipient deleted", device_token);
+                   break;            
                 case "transfer.created":
-                   notify.sendPushNotification("Transfer created", device_token);            
+                   notify.sendPushNotification("Transfer created", device_token);
+                   break;            
                 case "transfer.updated":
-                   notify.sendPushNotification("Transfer updated", device_token);            
+                   notify.sendPushNotification("Transfer updated", device_token);
+                   break;            
                 case "transfer.paid":
-                   notify.sendPushNotification("Transfer paid", device_token);            
+                   notify.sendPushNotification("Transfer paid", device_token);
+                   break;            
                 case "transfer.failed":
-                   notify.sendPushNotification("Transfer failed", device_token); 
+                   notify.sendPushNotification("Transfer failed", device_token);
+                   break; 
                 case "bitcoin.receiver.transaction.created":
-                   notify.sendPushNotification("Bitcoin receiver transaction created", device_token); 
+                   notify.sendPushNotification("Bitcoin receiver transaction created", device_token);
+                   break; 
                 case "bitcoin.receiver.created":
-                   notify.sendPushNotification("Bitcoin receiver created", device_token); 
+                   notify.sendPushNotification("Bitcoin receiver created", device_token);
+                   break; 
                 case "bitcoin.receiver.filled":
-                   notify.sendPushNotification("Bitcoin receiver filled", device_token); 
+                   notify.sendPushNotification("Bitcoin receiver filled", device_token);
+                   break; 
                 default:
-                   notify.sendPushNotification(evt, device_token);            
+                   notify.sendPushNotification(evt, device_token);
+                   break;            
               }
             } else {
               //notify.sendPushNotification("Could not find user!");
